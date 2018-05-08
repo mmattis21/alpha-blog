a47e662757b5c0a238bc36162f97c555a30dc660
Matt <MttMattis@gmail.com>
2018-05-08 17:52:17 -0400

    fun with articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,30 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else 
+      render 'new'  #or render :new
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was usccessfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
