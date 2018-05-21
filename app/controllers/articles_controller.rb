ce9ec36d17c7d690e0b7cd95cda88811ff4b552b
mmattis21 <mttmattis@gmail.com>
2018-05-21 17:45:37 -0400

    added article styling to display user created and updated at

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  
+
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
@@ -12,15 +12,15 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    article.user = User.first
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
-    else 
+    else
       render 'new'  #or render :new
     end
   end
-  
+
   def update
     if @article.update(article_params)
       flash[:success] = "Article was usccessfully updated"
@@ -29,26 +29,26 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def edit
   end
-  
+
   def show
   end
-  
-  def destroy 
+
+  def destroy
     @article.destroy
     flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
-    
+
   private
     def set_article
       @article = Article.find(params[:id])
     end
-  
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
-  
+
 end
