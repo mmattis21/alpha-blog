c49932cef89b187d48e0e1dda15dc067d191f92c
mmattis21 <mttmattis@gmail.com>
2018-05-22 14:00:36 -0400

    updated navigation bar and user permissions

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new] #ALTERNATIVE post 'users', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
