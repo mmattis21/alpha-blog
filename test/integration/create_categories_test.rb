377cccac1c57158a4d10c4332794beaa83aa2eeb
mmattis21 <mttmattis@gmail.com>
2018-05-24 21:37:12 -0400

    Fix list categories bug

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
   test "get new category form and create category" do
     get new_category_path
     assert_template 'categories/new'
